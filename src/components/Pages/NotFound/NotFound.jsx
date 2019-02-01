3da2bcaebf62af356b4f3546495b0b3d9ed8d1d8
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-01 16:42:52 +0530

    Added Some style for notfound page

--- src/components/Pages/NotFound/NotFound.jsx
+++ src/components/Pages/NotFound/NotFound.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./NotFound.css";
 
 const NotFound = () => {
     return (
         <div className="not-found">
             <h1>Sorry!! Your Query Isn't Found</h1>
-            <h3>Go Back to Home </h3>
+            <h3>
+                Go Back to <Link to="/">Home</Link>{" "}
+            </h3>
         </div>
     );
 };
