3538c7c5430a11fab022a2abdc342a3b85a2f357
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-01 21:03:06 +0530

    Added Scrolll button in movie page

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ScrollToTop from "react-scroll-up";
 
 import Spinner from "./../Pages/Spinner/Spinner";
 import Navigation from "./../Pages/Navigation/Navigation";
@@ -7,6 +8,8 @@ import MovieInfo from "./../Pages/MovieInfo/MovieInfo";
 import MovieInfoBar from "./../Pages/MovieInfoBar/MovieInfoBar";
 import FourGrid from "./../Pages/FourGrid/FourGrid";
 import Actor from "./../Pages/Actor/Actor";
+
+import "./Movie.css";
 
 const Movie = ({ movie, directors, actors, loading }) => {
     if (movie && directors && actors) {
@@ -24,6 +27,11 @@ const Movie = ({ movie, directors, actors, loading }) => {
                         <Actor key={actor.id} actor={actor} />
                     ))}
                 </FourGrid>
+                <ScrollToTop showUnder={160}>
+                    <span>
+                        <i className="fas fa-arrow-circle-up fa-3x movie-top" />
+                    </span>
+                </ScrollToTop>
                 {!actors && !loading ? <h1>No Movie Found</h1> : null}
             </React.Fragment>
         );
