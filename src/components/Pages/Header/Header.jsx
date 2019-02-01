7de1ea273868aadf17e8653b5b64585442640eb6
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-02-01 08:38:34 +0530

    Added Header section

--- src/components/Pages/Header/Header.jsx
+++ src/components/Pages/Header/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return <h1>I am Header</h1>;
+    return (
+        <header className="header">
+            <div>
+                <img
+                    src="./images/reactMovie_logo.png"
+                    alt=""
+                    className="react-movie"
+                />
+            </div>
+            <div>
+                <img
+                    src="./images/the_mdb_logo.png"
+                    alt=""
+                    className="rmdb-tmdb-logo"
+                />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
